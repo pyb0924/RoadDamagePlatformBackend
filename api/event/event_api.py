cbd674b7b9461ce848f767b9627623bc1a587e30
cckkk <653939262@qq.com>
2022-11-18 19:23:52 +0800

    git

--- api/event/event_api.py
+++ api/event/event_api.py
@@ -19,7 +19,7 @@ now_image_path = './'
 
 
 @event_router.post("", dependencies=[Depends(get_db)])  # 上传图片，建立新养护事件
-async def new_event(type: int, longitude: float, latitude: float, position: str, user: str, notes: str,
+async def new_event(type: int, longitude: float, latitude: float, address: str, user: str, notes: str,
                     file: List[UploadFile] = File(...)):
     try:
         filenamelist = []
@@ -31,18 +31,19 @@ async def new_event(type: int, longitude: float, latitude: float, position: str,
                 f.write(await a.read())
                 # if type == 0:#如果是用坑或者裂痕
                 # imagetest.image_seg(filename)
-        img_service.new_event(type, longitude, latitude, position, user, notes, filenamelist)
+        img_service.new_event(type, longitude, latitude, address, user, notes, filenamelist)
         return create_response(200, "新建养护事件成功", {})
-    except:
+    except Exception as e:
+        logger.error(e)
         raise APIException(404, "事件信息输入不全或格式错误")
 
 
 @event_router.get("", dependencies=[Depends(get_db)])  # 按事件的完工条件或位置查询
 async def search_event(type: int = None, min_longitude: float = None, max_longitude: float = None,
-                       min_latitude: float = None, max_latitude: float = None, position: str = None,
+                       min_latitude: float = None, max_latitude: float = None, address: str = None,
                        status: int = None):
     try:
-        event_list = img_service.search_event(type, min_longitude, max_longitude, min_latitude, max_latitude, position,
+        event_list = img_service.search_event(type, min_longitude, max_longitude, min_latitude, max_latitude, address,
                                               status)
         return create_response(200, "查询事件成功", event_list)
     except Exception as e:
@@ -65,7 +66,8 @@ async def update_event(event_id: str, status: int, user: str, notes: str, file: 
             return create_response(200, "事件状态修改成功", {})
         else:
             raise APIException(404, "非法修改状态")
-    except:
+    except Exception as e:
+        logger.error(e)
         raise APIException(404, "事件信息输入不全或格式错误")
 
 
@@ -77,7 +79,8 @@ async def update_event(event_id: str, status: int, user: str, notes: str):
             return create_response(200, "事件状态修改成功", {})
         else:
             raise APIException(404, "非法修改状态")
-    except:
+    except Exception as e:
+        logger.error(e)
         raise APIException(404, "事件信息输入不全或格式错误")
 
 
@@ -86,7 +89,8 @@ async def get_image(log_id: str):
     try:
         img_list = img_service.get_img_by_log(log_id)
         return create_response(200, "图片获取成功", img_list)
-    except:
+    except Exception as e:
+        logger.error(e)
         raise APIException(404, "不存在对应日志")
 
 
@@ -95,5 +99,6 @@ async def get_event_by_id(event_id: str):
     try:
         data = vars(Event.get(Event.event_id == event_id))['__data__']
         return create_response(200, "事件信息获取成功", data)
-    except:
+    except Exception as e:
+        logger.error(e)
         raise APIException(404, "不存在对应事件")
